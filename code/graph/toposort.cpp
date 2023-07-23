ef4b62e024cb40d4ee7ea81329657040f8d8d7ff
paulohltc <phltc@cin.ufpe.br>
2023-07-23 15:22:44 -0300

    toposort fix

--- code/graph/toposort.cpp
+++ code/graph/toposort.cpp
@@ -2,14 +2,14 @@
 const int MX = 1e5+5;
 vector<int> adj[MX];
 vector<int> dep(MX,0); // dep[x] = quantos nós se ligam a x
-vector<int> order;
 int v;
 
-bool toposort(){
-	order.clear();
+vector<int> toposort(){
+	vector<int> order;
+	queue<int> q;
 	for(int i = 0; i < v; i++)
 		if(dep[i] == 0) q.push(i);
-	if(q.empty()) return 0;
+	if(q.empty()) return order;
 	while(!q.empty()){
 		int f = q.front(); q.pop();
 		order.PB(f);
@@ -17,5 +17,5 @@ bool toposort(){
 			if(--dep[nb] == 0)
 				q.push(nb);
 	}
-	return order.size() == v;
+	return order;
 }
