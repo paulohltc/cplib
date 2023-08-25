7fa37c8d785afd704d5370a4c33dee515fa888cf
paulohltc <phltc@cin.ufpe.br>
2023-08-25 16:21:56 -0300

    inverso modular

---
+++ code/math/extendedEuclidean.cpp
@@ -0,0 +1,17 @@
+int gcd(int a, int b, int& x, int& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+// inverso modular de a
+int inv, y;
+int g = gcd(a,mod,inv,y);
+inv = (inv % m + m) % m; 
