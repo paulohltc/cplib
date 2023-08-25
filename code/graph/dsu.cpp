708ba76fa9ef3521f908be8c0e4ecc830926409e
Pedro <93168925+pedro-patriota@users.noreply.github.com>
2023-08-25 16:42:59 -0300

    Create dsu.cpp

---
+++ code/graph/dsu.cpp
@@ -0,0 +1,22 @@
+struct DSU{
+	vector<int> p;
+	vector<int> sz;
+	int n;
+	DSU(int nodes){
+		p.resize(nodes);
+		sz.resize(nodes,1);
+		iota(begin(p), end(p), 0);
+	}
+	int size(int a){ return sz[root(a)]; }
+	int root(int a){ return p[a] = (p[a] == a ? a : root(p[a])); }
+	bool unite(int a, int b){
+		int ra = root(a), rb = root(b);
+		if(ra != rb){
+			if(sz[ra] < sz[rb]) swap(ra,rb);
+			p[rb] = ra;
+			sz[ra] += sz[rb];
+			return 1;
+		}
+		return 0;
+	}
+};
