e5a62f18abd54c28d3e91ced0f5b19b492be8931
paulohltc <phltc@cin.ufpe.br>
2024-02-17 20:23:45 -0300

    lca

--- code/graph/lca.cpp
+++ code/graph/lca.cpp
@@ -1,22 +1,33 @@
-int par[ms][mlg+1], lvl[ms];
-void dfs(int v, int p, int l = 0) { // chamar como dfs(root, root)
-  lvl[v] = l;
-  par[v][0] = p;
-  for(int k = 1; k <= mlg; k++) {
-    par[v][k] = par[par[v][k-1]][k-1];
-  }
-  for(int u : g[v]) {
-    if(u != p) dfs(u, v, l + 1);
-  }
+const int mxn = 2e5+5;
+const int LOG = 22;
+int tin[mxn], tout[mxn];
+vector<vector<int>> up; // up[v][k] = 2^k-esimo ancestor de v
+vector<int> g[mxn];
+int timer = 0;
+void dfs(int u, int p){
+    tin[u] = ++timer;
+    up[u][0] = p;
+    for(int i = 1; i <= LOG; i++){
+        up[u][i] = up[ up[u][i-1] ][i-1];
+    }
+    for(int v : g[u]){
+        if(v != u && !tin[v])
+            dfs(v,u);
+    }
+    tout[u] = ++timer;
 }
-int lca(int a, int b) {
-  if(lvl[b] > lvl[a]) swap(a, b);
-  for(int i = mlg; i >= 0; i--) {
-    if(lvl[a] - (1 << i) >= lvl[b]) a = par[a][i];
-  }
-  if(a == b) return a;
-  for(int i = mlg; i >= 0; i--) {
-    if(par[a][i] != par[b][i]) a = par[a][i], b = par[b][i];
-  }
-  return par[a][0];
+ 
+bool is_ancestor(int u, int v){
+    return tin[u] <= tin[v] && tout[u] >= tout[v];
+}
+ 
+int lca(int a, int b){
+    if(is_ancestor(a,b)) return a;
+    if(is_ancestor(b,a)) return b;
+    for(int i = LOG; i >= 0; i--){
+        if(!is_ancestor(up[a][i], b)){
+            a = up[a][i];
+        }
+    }
+    return up[a][0];
 }
