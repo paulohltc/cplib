d9a771d9073eff22303208469439c877bb5e0662
paulohltc <phltc@cin.ufpe.br>
2024-03-03 19:28:52 -0300

    rabin karp double hashing

--- code/string/rabinkarp.cpp
+++ code/string/rabinkarp.cpp
@@ -1,27 +1,40 @@
 // Rabin Karp
+const ll base = 997;
+const ll mod[] ={1000000007, 1000000009};
+using Hash = pair<ll,ll>;
+const int str_mxsz = 1e5+2;
+ll pot[str_mxsz][2];
+void buildPots(){ // lembrar de chamar essa funcao
+	pot[0][0] = 1;
+	pot[0][1] = 1;
+	for(int i = 1; i < str_mxsz; i++)
+		for(int j = 0; j < 2; j++)
+			pot[i][j] = (pot[i-1][j]*base) % mod[j];
+}
+
 class RabinKarp{
 public:
-	ll base, mod, sz;
 	string s;
-	vector<ll> pot, has;
-	RabinKarp(const string& str,ll b = 997, ll m = 1e9 + 7)
-	: base(b), mod(m), s(str){
-		sz = str.length();
-		pot.resize(sz+1);
-		has.resize(sz+1);
+	int sz;
+	vector<vector<ll>> has;
+	RabinKarp(){}
+	RabinKarp(const string& str): s(str){
+		sz = str.size();
+		has.assign(sz+1,vector<ll>(2));
 		build();
 	}
 	void build(){
-		pot[0] = 1;
-		has[0] = s[0];
-		for(int i = 1; i < sz; i++){
-			pot[i] = (pot[i-1]*base) % mod;
-			has[i] = ((has[i-1]*base)+s[i])%mod;
-		}
+		has[0] = {s[0],s[0]};
+		for(int i = 1; i < sz; i++)
+			for(int j = 0; j < 2; j++)
+				has[i][j] = ((has[i-1][j]*base)+s[i])%mod[j];
 	}
-	ll getKey(ll l, ll r){ // inclusivo
-		ll ans = has[r];
-		if(l > 0) ans = (((ans - pot[r-l+1]*has[l-1])%mod + mod)%mod);
+	Hash getKey(ll l, ll r){ // inclusivo
+		Hash ans = {has[r][0],has[r][1]};
+		if(l > 0){
+			ans.first = (((ans.first - pot[r-l+1][0]*has[l-1][0])%mod[0] + mod[0])%mod[0]);
+			ans.second = (((ans.second - pot[r-l+1][1]*has[l-1][1])%mod[1] + mod[1])%mod[1]);
+		} 
 		return ans;
 	}
 };
