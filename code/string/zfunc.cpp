4d8489106034e19f7801d8306177f4e2d7051f7e
paulohltc <phltc@cin.ufpe.br>
2024-05-23 10:13:03 -0300

    add zfunc

--- code/string/zfunc.cpp
+++ code/string/zfunc.cpp
@@ -1,1 +1,10 @@
-// vai ficar p dps
+vector<int> Zfunction(string &s){
+	int n = s.size();
+	vector<int> z (n, 0);
+	for(int i=1, l=0, r=0; i<n;  i++)	{
+		if(i <= r) z[i] = min(z[i-l], r-i+1);
+		while(z[i] + i < n && s[z[i]] == s[i+z[i]]) z[i]++;
+		if(r < i+z[i]-1) l = i, r = i+z[i]-1;
+	}
+	return z;
+}
