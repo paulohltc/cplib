c6981b5995aced7a6391d2609757f541eae37308
paulohltc <phltc@cin.ufpe.br>
2024-06-13 22:25:48 -0300

    update ternary search

--- code/etc/ternary_search.cpp
+++ code/etc/ternary_search.cpp
@@ -5,16 +5,15 @@ double f(double t){
 	// __/    \__
 }
 
-double tern_search(double l, double r){ // achar o maximo
-	double eps = 1e-8;
-	while(r - l > eps){
-		double m1 = l + (r-l)/3;
-		double m2 = r - (r-l)/3;
-		double f1 = f(m1), f2 = f(m2);
-		if(f1 < f2)	l = m1; // (m1,r)
-		else r = m2; // (l,m2);
-	}
-	return max(f(l),f(r));
+double tern_search(double l, double r){
+  for(int it = 0; it < 300; it++){ 
+    double m1 = l + (r-l)/3;
+    double m2 = r - (r-l)/3;
+    double f1 = f(m1), f2 = f(m2);
+    if(f1 < f2) l = m1; 
+    else r = m2; 
+  } 
+  return max(f(l),f(r)); 
 }
 // retorna mais a esquerda no empate
 int int_tern_search(int l, int r){
