4dc42cb3f6d2a9c581565e22eaab1f92f2a8c882
paulohltc <phltc@cin.ufpe.br>
2024-06-16 15:39:01 -0300

    golden section search

--- code/etc/ternary_search.cpp
+++ code/etc/ternary_search.cpp
@@ -10,20 +10,35 @@ double tern_search(double l, double r){
     double m1 = l + (r-l)/3;
     double m2 = r - (r-l)/3;
     double f1 = f(m1), f2 = f(m2);
-    if(f1 < f2) l = m1; 
+    if(f1 < f2) l = m1; //change to > to find maximum
     else r = m2; 
   } 
-  return max(f(l),f(r)); 
+  return l; 
+}
+// golden section search
+double gss(double a, double b) {
+  const double r = (sqrt(5)-1)/2, eps = 1e-7;
+  double x1 = b - r*(b-a), x2 = a + r*(b-a);
+  double f1 = f(x1), f2 = f(x2);
+  for(int it = 0; it < 250 && b-a > eps; it++)
+    if (f1 < f2) { //change to > to find maximum
+      b = x2; x2 = x1; f2 = f1;
+      x1 = b - r*(b-a); f1 = f(x1);
+    } else {
+      a = x1; x1 = x2; f1 = f2;
+      x2 = a + r*(b-a); f2 = f(x2);
+    }
+  return a;
 }
 // retorna mais a esquerda no empate
 int int_tern_search(int l, int r){
 	int lo = l - 1, hi = r;
 	while(hi - lo > 1){
 		int m = (lo+hi)/2;
-		if(f(m) >= f(m+1)){ // decrescendo
-			hi = m;
-		}else{ // crescendo;
+		if(f(m) < f(m+1)){ // 
 			lo = m;
+		}else{ 
+			hi = m;
 		}
 	}
 	return lo + 1;
