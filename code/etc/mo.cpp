bc9b76aff2611ab0b6ffa7c20354b1e5453e24f4
paulohltc <phltc@cin.ufpe.br>
2024-08-30 10:42:50 -0300

    lib ok

--- code/etc/mo.cpp
+++ code/etc/mo.cpp
@@ -1,37 +1,42 @@
 // Mo apelao
 // Ordering based on the Hilbert curve
-inline int64_t hilbertOrder(int x, int y, int pow, int rotate){
-    if(pow == 0) return 0;
-    int hpow = 1 << (pow - 1);
-    int seg = (x < hpow) ? ( (y < hpow) ? 0 : 3) : ( (y < hpow) ? 1 : 2);
-    seg = (seg + rotate) & 3;
-    const int rotateDelta[4] = {3, 0, 0, 1};
-    int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
-    int nrot = (rotate + rotateDelta[seg]) & 3;
-    int64_t subSquareSize = int64_t(1) << (2*pow - 2);
-    int64_t ans = seg * subSquareSize;
-    int64_t add = hilbertOrder(nx, ny, pow - 1, nrot);
-    ans += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
-    return ans;
-}
- 
+// inline int64_t hilbertOrder(int x, int y, int pow, int rotate){
+//     if(pow == 0) return 0;
+//     int hpow = 1 << (pow - 1);
+//     int seg = (x < hpow) ? ( (y < hpow) ? 0 : 3) : ( (y < hpow) ? 1 : 2);
+//     seg = (seg + rotate) & 3;
+//     const int rotateDelta[4] = {3, 0, 0, 1};
+//     int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
+//     int nrot = (rotate + rotateDelta[seg]) & 3;
+//     int64_t subSquareSize = int64_t(1) << (2*pow - 2);
+//     int64_t ans = seg * subSquareSize;
+//     int64_t add = hilbertOrder(nx, ny, pow - 1, nrot);
+//     ans += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
+//     return ans;
+// }
+// struct Query{
+//     int l, r, idx;
+//     int64_t ord;
+//     Query(int l, int r, int idx) : l(l), r(r), idx(idx) {
+//         ord = hilbertOrder(l, r, 21, 0);
+//     }
+//     bool operator < (Query &other){
+//         return ord < other.ord;
+//     }
+// };
 const int MXN = 2e5;
+const int B = sqrt(MXN) + 1;
+struct Query {
+    int l, r, idx;
+    bool operator<(Query other) const{
+      return make_pair(l / B, ((l/B) & 1) ? -r : r) < make_pair(other.l / B, ((other.l/B) & 1) ? -other.r : other.r);
+    }
+};
+
 ll a[MXN];
 ll resp = 0;
 void add(int x);
 void remove(int x);
-
-struct Query{
-    int l, r, idx;
-    int64_t ord;
-    Query(int l, int r, int idx) : l(l), r(r), idx(idx) {
-        ord = hilbertOrder(l, r, 21, 0);
-    }
-    bool operator < (Query &other){
-        return ord < other.ord;
-    }
-};
- 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
