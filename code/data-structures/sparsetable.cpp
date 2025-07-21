2f153b46a18bf92ef9145f5993c96129cf563f73
Paulo Correia <phltc@cin.ufpe.br>
2025-07-21 20:16:07 -0300

    refactor sparse table

--- code/data-structures/sparsetable.cpp
+++ code/data-structures/sparsetable.cpp
@@ -1,25 +1,25 @@
-vector<vector<ll>> table;
-vector<ll> lg2;
-void build(int n, vector<ll> v) {
-  lg2.resize(n + 1);
-  lg2[1] = 0;
-  for (int i = 2; i <= n; i++) {
-    lg2[i] = lg2[i >> 1] + 1;
-  }
-  table.resize(lg2[n] + 1);
-  for (int i = 0; i < lg2[n] + 1; i++) {
-    table[i].resize(n + 1);
-  }
-  for (int i = 0; i < n; i++) {
-    table[0][i] = v[i];
-  }
-  for (int i = 0; i < lg2[n]; i++) {    for (int j = 0; j < n; j++) {
-      if (j + (1 << i) >= n) break;
-      table[i + 1][j] = min(table[i][j], table[i][j + (1 << i)]);
+// use const, principalmente no log
+// log = maior msb a[i]
+const int LOG = 20;
+// query [L,R] 
+template<typename T>
+struct SparseTable{
+  int n;
+  vector<vector<T>> table;
+  SparseTable(){}
+  SparseTable(vector<T>& v){
+    n = v.size();
+    table = vector(LOG + 1, vector<T>(n));
+    table[0] = v;
+    for(int lg = 0; lg < LOG; lg++){
+      for(int i = 0; i < n; i++){
+        if (j + (1 << i) >= n) break;
+        table[i + 1][j] = min(table[i][j], table[i][j + (1 << i)]);
+      }
     }
   }
-}
-ll get(int l, int r) { // (l,r) inclusivo
-  int k = lg2[r - l + 1];
-  return min(table[k][l], table[k][r - (1 << k) + 1]);
-}
+  T qry(int l, int r){
+    int k = 31 - __builtin_clz(r-l+1);
+    return min(table[k][l], table[k][r - (1 << k) + 1]);
+  }
+};
