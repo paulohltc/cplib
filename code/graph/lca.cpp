9b4a7081e3c51557bd98d7c25178c91fc668ac5c
Paulo Correia <phltc@cin.ufpe.br>
2026-01-31 20:18:30 -0300

    refactor(lca): uses cache friendly binary lifting

--- code/graph/lca.cpp
+++ code/graph/lca.cpp
@@ -2,35 +2,37 @@ const int mxn = 2e5+5;
 const int LOG = 22;
 int n, q;
 int tin[mxn], tout[mxn];
-vector<vector<int>> up; // up[v][k] = 2^k-esimo ancestor de v
+int up[LOG][mxn]; 
 vector<int> g[mxn];
 int lvl[mxn];
 int timer = 0;
 void dfs(int u, int p){
     tin[u] = ++timer;
     lvl[u] = lvl[p] + 1;
-    up[u][0] = p;
-    for(int i = 1; i <= LOG; i++){
-        up[u][i] = up[ up[u][i-1] ][i-1];
-    }
-    for(int v : g[u]){
-        if(v != u && !tin[v])
-            dfs(v,u);
-    }
+    up[0][u] = p;
+    for(int v : g[u]) if(v != p)
+        dfs(v,u);
     tout[u] = ++timer;
 }
  
 bool is_ancestor(int u, int v){
     return tin[u] <= tin[v] && tout[u] >= tout[v];
 }
+
+void buildBL(){
+    for(int lg = 1; lg < LOG; lg++)
+        for(int u = 0; u < n; u++)
+            up[lg][u] = up[lg-1][ up[lg-1][u] ];
+}
  
+// lembrar de chamar dfs + buildbl antes de usar
 int lca(int a, int b){
     if(is_ancestor(a,b)) return a;
     if(is_ancestor(b,a)) return b;
-    for(int i = LOG; i >= 0; i--){
-        if(!is_ancestor(up[a][i], b)){
-            a = up[a][i];
+    for(int i = LOG - 1; i >= 0; i--){
+        if(!is_ancestor(up[i][a], b)){
+            a = up[i][a];
         }
     }
-    return up[a][0];
+    return up[0][a];
 }
