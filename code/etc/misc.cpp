0f0947baed72f58440b22075e3db6ee3f0cee4dd
Paulo Correia <phltc@cin.ufpe.br>
2026-08-26 19:38:49 -0300

    refactor(lca): add maior aresta no caminho

---
+++ code/etc/misc.cpp
@@ -0,0 +1,3 @@
+pair<int,int> rot90HGrid(pair<int,int> p, int n){
+    return {j, n - i};
+}
--- code/graph/lca.cpp
+++ code/graph/lca.cpp
@@ -1,38 +1,70 @@
-const int mxn = 2e5+5;
-const int LOG = 22;
+// lembrar de chamar dfs + buildBL antes de usar
+const int mxn = 1e5+5;
+const int LOG = 18;
 int n, q;
-int tin[mxn], tout[mxn];
-int up[LOG][mxn]; 
-vector<int> g[mxn];
-int lvl[mxn];
+int tin[mxn], tout[mxn], lvl[mxn];
+int up[LOG][mxn], maior[LOG][mxn]; 
+vector<pair<int,int>> g[mxn];
 int timer = 0;
-void dfs(int u, int p){
-    tin[u] = ++timer;
-    lvl[u] = lvl[p] + 1;
-    up[0][u] = p;
-    for(int v : g[u]) if(v != p)
-        dfs(v,u);
-    tout[u] = ++timer;
+
+void dfs(int u, int p, int c){
+  tin[u] = ++timer;
+  lvl[u] = lvl[p] + 1;
+  up[0][u] = p;
+  maior[0][u] = c;
+  for(auto [v,c] : g[u]) if(v != p)
+    dfs(v,u,c);
+  tout[u] = ++timer;
 }
  
 bool is_ancestor(int u, int v){
-    return tin[u] <= tin[v] && tout[u] >= tout[v];
+  return tin[u] <= tin[v] && tout[u] >= tout[v];
 }
 
 void buildBL(){
-    for(int lg = 1; lg < LOG; lg++)
-        for(int u = 0; u < n; u++)
-            up[lg][u] = up[lg-1][ up[lg-1][u] ];
+  for(int lg = 1; lg < LOG; lg++){
+    for(int u = 0; u < n; u++){
+      up[lg][u] = up[lg-1][ up[lg-1][u] ];
+      maior[lg][u] = max(
+        maior[lg-1][u],
+        maior[lg-1][ up[lg-1][u] ]
+      );
+    }
+  }   
 }
  
-// lembrar de chamar dfs + buildbl antes de usar
 int lca(int a, int b){
-    if(is_ancestor(a,b)) return a;
-    if(is_ancestor(b,a)) return b;
-    for(int i = LOG - 1; i >= 0; i--){
-        if(!is_ancestor(up[i][a], b)){
-            a = up[i][a];
-        }
+  if(is_ancestor(a,b)) return a;
+  if(is_ancestor(b,a)) return b;
+  for(int i = LOG - 1; i >= 0; i--){
+    if(!is_ancestor(up[i][a], b)){
+      a = up[i][a];
     }
-    return up[0][a];
+  }
+  return up[0][a];
+}
+
+int lift(int &u, int k) {
+  int ans = 0;
+
+  for (int i = LOG - 1; i >= 0; i--) {
+    if (k & (1 << i)) {
+      ans = max(ans, maior[i][u]);
+      u = up[i][u];
+    }
+  }
+
+  return ans;
+}
+
+int getMaior(int u, int v) {
+  int l = lca(u, v);
+
+  int ans = 0;
+  int du = lvl[u] - lvl[l];
+  int dv = lvl[v] - lvl[l];
+  ans = max(ans, lift(u, du));
+  ans = max(ans, lift(v, dv));
+
+  return ans;
 }
