f5ef8114ff170d071039726b041b7010cc4fcdd7
paulohltc <55456082+paulohltc@users.noreply.github.com>
2026-09-06 19:14:25 -0300

    Update fastexp.cpp

--- code/math/fastexp.cpp
+++ code/math/fastexp.cpp
@@ -11,33 +11,38 @@ ll fexpll(ll a, ll n){
 	}
 	return ans;
 }
-// matriz quadrada
-class Matrix{ 
-	public:
-	vector<vector<ll>> mat;
-	int m;
-	Matrix(int m): m(m){
-		mat.resize(m);
-		for(int i = 0; i < m; i++) mat[i].resize(m,0);
-	}
-	Matrix operator * (const Matrix& rhs){
-		Matrix ans = Matrix(m);
-		for(int i = 0; i < m; i++)
-			for(int j = 0; j < m; j++)
-				for(int k = 0; k < m; k++)
-					ans.mat[i][j] = (ans.mat[i][j] + (mat[i][k] * rhs.mat[k][j]) % mod) % mod;
-		return ans;
-	}
+
+template<int MOD>
+class Matrix {
+public:
+    vector<vector<int>> mat;
+    int m;
+
+    Matrix(int m) : m(m), mat(m, vector<int>(m, 0)) {}
+
+    Matrix operator*(const Matrix& rhs) const {
+        Matrix ans(m);
+        for(int i = 0; i < m; i++) {
+            for(int k = 0; k < m; k++) {
+                for(int j = 0; j < m; j++) {
+                    ans.mat[i][j] += 1LL * mat[i][k] * rhs.mat[k][j] % MOD;
+                    if(ans.mat[i][j] >= MOD) ans.mat[i][j] -= MOD;
+                }
+            }
+        }
+
+        return ans;
+    }
 };
 
-Matrix fexp(Matrix a, ll n){
-	int m = a.m;
-	Matrix ans = Matrix(m);
-	for(int i = 0; i < m; i++) ans.mat[i][i] = 1;
-	while(n){
-		if(n & 1) ans = ans * a;
-		a = a * a;
-		n >>= 1;
-	}
-	return ans;
+template<int MOD>
+Matrix<MOD> fexp(Matrix<MOD> a, long long n) {
+    Matrix<MOD> ans(a.m);
+    for(int i = 0; i < a.m; i++) ans.mat[i][i] = 1;
+    while(n) {
+        if(n & 1) ans = ans * a;
+        a = a * a;
+        n >>= 1;
+    }
+    return ans;
 }
